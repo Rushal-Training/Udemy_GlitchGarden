855cff71cb51ee6d419509ff8efeb259676c0bf8
Rushal <TheRushal@gmail.com>
2019-05-03 15:02:45 -0500

    Start of animations video

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+	[SerializeField] int timeToWait = 3;
+
+	int currentSceneIndex;
+
+    void Start()
+    {
+		currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+		if( currentSceneIndex == 0 )
+		{
+			StartCoroutine( WaitForTime() );
+		}
+    }
+
+	IEnumerator WaitForTime()
+	{
+		yield return new WaitForSeconds( timeToWait );
+		LoadNextScene();
+	}
+
+	public void LoadNextScene()
+	{
+		SceneManager.LoadScene( currentSceneIndex + 1 );
+	}
+}
